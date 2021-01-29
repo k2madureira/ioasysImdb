fcd2d8ac3c0b92b808463468ebfb2df356c6e3a0
Lenilson <lenilsonmadureira@gmail.com>
2021-01-29 16:43:59 -0300

    core review validation

--- src/validations/score.validation.js
+++ src/validations/score.validation.js
@@ -2,7 +2,7 @@ const yup = require('yup');
 
 const create = {
   body: yup.object().shape({
-    score: yup.number().positive().min(0).max(4).required(),
+    score: yup.number().min(0).max(4).required(),
   }),
   params: yup.object().shape({
     id: yup.string().uuid('id param with invalid uuid').required(),

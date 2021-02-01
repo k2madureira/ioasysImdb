d6b699420dcdcacf3cca931de81595e186f40e27
Lenilson <lenilsonmadureira@gmail.com>
2021-02-01 15:39:53 -0300

    genre validation

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const userService = require('./users');
 const movieService = require('./movie');
 const scoreService = require('./score');
+const genreService = require('./genre');
 
 module.exports = {
   userService,
   movieService,
   scoreService,
+  genreService,
 };
--- src/validations/index.js
+++ src/validations/index.js
@@ -2,10 +2,12 @@ const { users } = require('./user.validation');
 const { isAdmin } = require('./admin.validation');
 const { movies } = require('./movie.validation');
 const { scores } = require('./score.validation');
+const { genres } = require('./genre.validation');
 
 module.exports.validationSchemas = {
   users,
   movies,
   isAdmin,
   scores,
+  genres,
 };

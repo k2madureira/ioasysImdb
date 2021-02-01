01741e59faf647c26f55fbd6f142ed577b412821
Lenilson <lenilsonmadureira@gmail.com>
2021-02-01 13:55:45 -0300

    model genre

---
+++ src/models/genre.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const Genre = sequelize.define(
+    'Genre',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      genre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'genres',
+      underscored: true,
+    },
+  );
+
+  return Genre;
+};

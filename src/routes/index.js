67751b84ab1c2eacc9158f35e334d9dc0a5b41e9
Lenilson <lenilsonmadureira@gmail.com>
2021-02-05 09:53:00 -0300

    top_stars

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { users } = require('./users.routes');
 const { movies } = require('./movies.routes');
 const { scores } = require('./scores.routes');
 const { genres } = require('./genres.routes');
+const { reports } = require('./reports.routes');
 
 module.exports = {
   users,
   movies,
   scores,
   genres,
+  reports,
 };

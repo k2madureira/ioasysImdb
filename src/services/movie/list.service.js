9b23daeb491404fc85177f567b6aebe36385c494
Lenilson <lenilsonmadureira@gmail.com>
2021-02-08 12:49:29 -0300

    detai / list movies

--- src/services/movie/list.service.js
+++ src/services/movie/list.service.js
@@ -14,12 +14,39 @@ module.exports.list = async (title, page, limit) => {
   );
 
   const allMovies = await movieRepository.findAll();
+
+  const treated = [];
+  movies.rows.map(movie => {
+    const sumScores = movie.scores.reduce((obj, elem) => {
+      if (!obj.soma) {
+        obj = {
+          soma: 0,
+          count: 0,
+          avg: 0,
+        };
+      }
+      obj.soma += elem.score;
+      obj.count += 1;
+      obj.avg = obj.soma / obj.count;
+      return obj;
+    }, {});
+
+    delete movie.dataValues.scores;
+
+    treated.push({
+      ...movie.dataValues,
+      total_votes: sumScores.count,
+      average_votes: sumScores.avg,
+    });
+    return movie;
+  });
+
   const totalMovies = allMovies.length;
 
   const totalPage = Math.ceil(totalMovies / limit);
 
   const response = {
-    movies: movies.rows,
+    movies: treated,
     pagination: {
       movies: totalMovies,
       limit,
